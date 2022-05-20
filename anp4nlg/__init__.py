0b46cc9342022ca715e36fe8f7ed98f8f3ac0e0b
vshulev <velizar.shulev@gmail.com>
2022-05-20 20:43:49 +0200

    Impl np as subclass of FairseqLanguageModel

--- anp4nlg/__init__.py
+++ anp4nlg/__init__.py
@@ -1,14 +1,15 @@
 from anp4nlg.models.np.neural_process import NeuralProcess, AttentiveNeuralProcess
 from anp4nlg.models.np.criterion import NeuralProcessCriterion
+# from anp4nlg.models.np_test.neural_process import NeuralProcess
+# from anp4nlg.criterions.neural_process import NeuralProcessCriterion
 
 from fairseq.models import register_model_architecture
-from fairseq.criterions import register_criterion
 
 
 @register_model_architecture('neural_process', 'neural_process_lm')
 def neural_process_arch(args):
     pass
 
-@register_model_architecture('attentive_neural_process', 'attentive_neural_process_lm')
-def attentive_neural_process_arch(args):
-    pass
+# @register_model_architecture('attentive_neural_process', 'attentive_neural_process_lm')
+# def attentive_neural_process_arch(args):
+#     pass
