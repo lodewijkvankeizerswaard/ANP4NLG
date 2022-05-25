f7d398f2ae454c285d5ef8de11c3b63bd2e9892e
vshulev <velizar.shulev@gmail.com>
2022-05-25 16:28:12 +0200

    Add scripts for bpe encoding

--- preprocess.sh
+++ preprocess.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-source activate anp4nlg_gpu
-
 TEXT=wikitext-103
 
-# python preprocess.py "$TEXT"
 fairseq-preprocess \
     --only-source \
-    --thresholdsrc=2 \
-    --tokenizer space \
     --task language_modeling \
-    --trainpref $TEXT/wiki.train.tokens \
-    --validpref $TEXT/wiki.valid.tokens \
-    --testpref $TEXT/wiki.test.tokens \
+    --trainpref $TEXT/wiki.train.tokens.bpe \
     --destdir data-bin/wikitext-103 \
     --workers 20
