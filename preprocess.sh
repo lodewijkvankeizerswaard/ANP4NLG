38203aeaa44307abebf10505880cd0f22827b87d
vshulev <velizar.shulev@gmail.com>
2022-05-25 16:34:25 +0200

    Add valid and test sets to preprocess command

--- preprocess.sh
+++ preprocess.sh
@@ -6,5 +6,7 @@ fairseq-preprocess \
     --only-source \
     --task language_modeling \
     --trainpref $TEXT/wiki.train.tokens.bpe \
+    --validpref $TEXT/wiki.valid.tokens.bpe \
+    --testpref $TEXT/wiki.test.tokens.bpe \
     --destdir data-bin/wikitext-103 \
     --workers 20
