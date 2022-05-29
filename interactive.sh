45b6140632864e64f4a832ed8e720849f308448f
vshulev <velizar.shulev@gmail.com>
2022-05-29 21:13:00 +0200

    Add script for sampling

--- interactive.sh
+++ interactive.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-fairseq-interactive data-bin/wikitext-103 \
-  --path checkpoints/transformer_wikitext-103/checkpoint_best.pt \
-  --user-dir anp4nlg \
+fairseq-interactive \
+  --path checkpoints/transformer_wikitext-103/checkpoint_last.pt \
+  data-bin/wikitext-103/ \
   --task language_modeling \
-  --self-target
+  --bpe subword_nmt \
+  --bpe-codes wikitext-103/codes.bpe \
+  --user-dir anp4nlg \
+  --self-target \
+  --sampling \
+  --sampling-topk 20 \
+  --no-repeat-ngram-size 3 \
+  --nbest 5 \
+  --temperature 1.5 \
+  --min-len 64 \
+  --sacrebleu
